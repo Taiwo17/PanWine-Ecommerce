2946cc97d5285a0de04192c2746576c877bac93a
taiwo17 <shoboadefowope2007@gmail.com>
2022-11-09 00:28:53 -0800

    LocalStorage Included

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -6,7 +6,9 @@ import { createSlice } from '@reduxjs/toolkit'
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-    cart: [],
+    cart: localStorage.getItem('cart')
+      ? JSON.parse(localStorage.getItem('cart'))
+      : [],
   },
   reducers: {
     addToCarts: (state, action) => {
@@ -16,18 +18,22 @@ const cartSlice = createSlice({
       itemInCart
         ? itemInCart.quantity++
         : state.cart.push({ ...action.payload, quantity: 1 })
+      localStorage.setItem('cart', JSON.stringify(state.cart))
     },
     incrementQuantity: (state, action) => {
       const item = state.cart.find((item) => item.id === action.payload)
       item.quantity++
+      localStorage.setItem('cart', JSON.stringify(state.cart))
     },
     decrementQuantity: (state, action) => {
       const item = state.cart.find((item) => item.id === action.payload)
       item.quantity === 1 ? (item.quantity = 1) : item.quantity--
+      localStorage.setItem('cart', JSON.stringify(state.cart))
     },
     removeQuantity: (state, action) => {
       const removeItem = state.cart.filter((item) => item.id !== action.payload)
       state.cart = removeItem
+      localStorage.setItem('cart', JSON.stringify(state.cart))
     },
   },
 })
